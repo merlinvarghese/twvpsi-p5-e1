c7782b86f785e177c9e049080ef41271802438ec
Nidhi Vashishth <vashishth.nidhi@gmail.com>
2019-08-28 15:15:57 +0530

    [Srividya,Nidhi] Refactor. tests

--- src/test/com/tw/vapasi/RectangleTest.java
+++ src/test/com/tw/vapasi/RectangleTest.java
@@ -1,47 +1,48 @@
 package com.tw.vapasi;
 
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class RectangleTest {
     @Test
-    void shouldReturn8With2And4() {
+    void expectArea8With4And2() {
         Rectangle rectangle = new Rectangle(4, 2);
         assertEquals(8, rectangle.area());
     }
 
     @Test
-    void shouldReturn18With2And9() {
+    void expectArea18With9And2() {
         Rectangle rectangle = new Rectangle(9, 2);
         assertEquals(18, rectangle.area());
     }
 
     @Test
-    void shouldReturn4Point5With2Point5And1Point8() {
+    void expectArea4Point5With2Point5And1Point8() {
         Rectangle rectangle = new Rectangle(2.5, 1.8);
         assertEquals(4.5, rectangle.area());
     }
 
     @Test
-    void shouldReturn12With4Point8And2Point5() {
+    void expectArea12With4Point8And2Point5() {
         Rectangle rectangle = new Rectangle(4.8, 2.5);
         assertEquals(12, rectangle.area());
     }
 
     @Test
-    void isPerimeter16With5And3(){
+    void expectPerimeter16With5And3() {
         Rectangle rectangle = new Rectangle(5, 3);
         assertEquals(16, rectangle.perimeter());
     }
 
     @Test
-    void isPerimeter14With4And3(){
-        Rectangle rectangle = new Rectangle(4, 3);
-        assertEquals(14, rectangle.perimeter());
+    void expectPerimeter15With4And3Point5() {
+        Rectangle rectangle = new Rectangle(4, 3.5);
+        assertEquals(15, rectangle.perimeter());
     }
 
-
+    @Test
+    void expectPerimeter18Point6With5Point5And3Point8() {
+        Rectangle rectangle = new Rectangle(5.5, 3.8);
+        assertEquals(18.6, rectangle.perimeter());
+    }
 }
