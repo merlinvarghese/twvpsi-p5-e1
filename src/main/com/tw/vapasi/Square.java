cb0dff2db539a1bcb8252551934c084dfbf15f4d
Nidhi Vashishth <vashishth.nidhi@gmail.com>
2019-08-28 15:43:43 +0530

    [Srividya,Nidhi] Add. initial test for Square

---
+++ src/main/com/tw/vapasi/Square.java
@@ -0,0 +1,15 @@
+package com.tw.vapasi;
+
+/**
+ * Square is a rectangle with all equal sides
+ */
+class Square extends Rectangle {
+
+    Square(double side) {
+        super(side, side);
+    }
+
+    double area() {
+        return super.area();
+    }
+}
